14defbe42e9dd5a2f0cda433fcb0a03c9bc7ea12
Tonny Bett <betttonny26@gmail.com>
2024-07-09 14:18:14 +0300

    challange 1

---
+++ chalnge 1/index.js
@@ -0,0 +1,17 @@
+let marks= prompt(parseInt("Enter your marks"));
+if (marks>=79) {
+    console.log("A")
+}
+else if (marks >=60 && marks <79) {
+    console.log("B")
+}
+else if (marks >=49 && marks <59) {
+  console.log("C") 
+}
+else if ( marks >=40 && marks<49){
+    console.log("D")
+}
+else {
+    console.log("E")
+}
+print (marks);
